a3bffac88fdbc1ce54ec5dd7ed49380a41d7d550
schulle0815 <andreas.schulze79@gmx.de>
2018-12-03 14:57:47 +0100

    added book list

---
+++ src/app/book-list/book-list.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import {Book, Thumbnail} from '../shared/book';
+
+@Component({
+  selector: 'bm-book-list',
+  templateUrl: './book-list.component.html',
+  styles: [],
+})
+export class BookListComponent implements OnInit {
+  private books: Array<Book>;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.books = [
+      new Book(
+          '9783864903571', 'Angular', ['Hans Wurst', 'Wans Hurst'], new Date(2017, 3, 1),
+          'Grundlagen, fortgeschrittene Techniken und Best Practices mit TypeScript - ab Angular 4, inkl. Native Script und Redux', 5,
+          [new Thumbnail('https://ng-buch.de/cover2.jpg', 'Buchcover')], 'Mit Angular wird alles besser!'),
+      new Book(
+          '9783864901546', 'AngularJS', ['Hans Wurst', 'Wans Hurst'], new Date(2014, 5, 29), 'eine praktische Einführung', 5,
+          [new Thumbnail('https://ng-buch.de/cover1.jpg', 'Buchcover')],
+          'AngularJS ist schon toll, aber mittlerweile auch ein bischen veraltet...')
+    ];
+  }
+}
---
+++ src/app/shared/book.ts
@@ -0,0 +1,8 @@
+import {Thumbnail} from './thumbnail';
+export {Thumbnail} from './thumbnail';
+
+export class Book {
+  constructor(
+      public isbn: string, public title: string, public authors: string[], public published: Date, public subtitle?: string,
+      public rating?: number, public thumbnails?: Array<Thumbnail>, public description?: string) {}
+}
